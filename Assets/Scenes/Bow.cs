b524bc4421f683cc42ef3e40ee4855d6cf485b4b
wtprk <144166814+wtprk@users.noreply.github.com>
2025-04-08 16:31:25 -0400

    bow stuff added

---
+++ Assets/Scenes/Bow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    public Animator anime;
+    public GameObject arrowPrefab;
+    public Transform startPos = null;
+    public Transform endPos = null;
+    public Transform notchPos = null;
+
+    public float pullValue = 0.0f;
+    public Transform hand = null;
+
+    public Arrow current;
+
+    void Awake()
+    {
+      anime = GetComponent<Animator>();   
+    }
+
+    void Start()
+    {
+        createArrow();   
+    }
+
+    public void createArrow(){
+        GameObject newArrow = Instantiate(arrowPrefab, notchPos);
+        newArrow.transform.SetParent(notchPos);
+        newArrow.transform.localPosition = new Vector3(0,0,0.425f);
+        newArrow.transform.localEulerAngles = Vector3.zero;
+        current = newArrow.GetComponent<Arrow>();
+    }
+
+    void Update()
+    {
+        if(!current || !hand)
+        {
+            return;
+        }
+        pullValue = calcPull(hand);
+        if(pullValue < 0.0f){
+            pullValue = 0.0f;
+        }
+        if(pullValue > 1.0f){
+            pullValue = 1.0f;
+        }
+
+        anime.SetFloat("Blend", pullValue);
+    }
+
+    public float calcPull(Transform hand){
+        return 0.0f;
+    }
+
+    public void Pull(Transform handPos){
+        
+    }
+
+    public void Release(){
+
+    }
+
+}
